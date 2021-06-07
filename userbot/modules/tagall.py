3ba29ef6c561c3d0739f4269212c97d7fbe92af6
coshgyn <74981823+coshgyn@users.noreply.github.com>
2021-06-07 12:57:52 +0000

    Update tagall.py

--- userbot/modules/tagall.py
+++ userbot/modules/tagall.py
@@ -7,9 +7,10 @@ from collections import deque
 from telethon import events
 from telethon.tl.types import ChannelParticipantsAdmins
 from asyncio import sleep
+from time import sleep
 from random import choice, getrandbits, randint
 from re import sub
-from userbot import CMD_HELP, bot
+from userbot import CMD_HELP, bot, SUDO_ID
 from userbot.events import register
 from userbot.modules.admin import get_user_from_event
 from userbot.cmdhelp import CmdHelp
@@ -27,6 +28,30 @@ async def _(event):
             leng += 1
     await event.reply(mentions)
     await event.delete()
+
+
+@register(outgoing=True, pattern="^.taq(.*)")
+@register(incoming=True, from_users=SUDO_ID, pattern="^.taq(.*)")
+async def tagallcmd(event):
+  if event.pattern_match.group(1):
+    tag = event.pattern_match.group(1)
+  else:
+    tag = ""
+  await event.delete()
+  tags = []
+  async for user in event.client.iter_participants(event.chat_id, 750):
+   tags.append(f"[{user.first_name}](tg://user?id={user.id})\n")
+  chunkss = list(chunks(tags, 6))
+  random.shuffle(chunkss)
+  for chunk in chunkss:
+   await event.client.send_message(event.chat_id, '\u2060'.join(chunk)+tag)
+   sleep(1.5)
+
+
+def chunks(lst, n):
+ for i in range(0, len(lst), n):
+  yield lst[i:i + n]
+
 
 @register(outgoing=True, pattern="^.admin")
 async def _(event):
